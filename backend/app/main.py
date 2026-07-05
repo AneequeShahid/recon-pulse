a5695a7bb38c5d7b1b0553a28bc8b6a8123b049a
Aneeque <aneequeshahid495@gmail.com>
2026-07-05 11:17:16 +0500

    add dotenv support in main entrypoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+# Load environment configuration variables
+load_dotenv()
+
 from app.routers import report, health
 
 app = FastAPI(

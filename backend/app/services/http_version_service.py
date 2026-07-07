794a5fe8a5d11f62cb6a3e852268d4f1e9cf32b4
Aneeque <aneequeshahid495@gmail.com>
2026-07-07 11:05:20 +0500

    add http version checker

--- backend/app/services/http_version_service.py
+++ backend/app/services/http_version_service.py
@@ -1,30 +1,13 @@
 import httpx
 from app.models import HTTPVersionInfo
 
-async def check_http_version(domain: str) -> HTTPVersionInfo:
+async def fetch_http_version(url: str) -> HTTPVersionInfo:
     try:
-        async with httpx.AsyncClient(http2=True, timeout=5, follow_redirects=True) as client:
-            res = await client.get(f"https://{domain}")
-            http_version = res.http_version
-            http2_support = http_version == "HTTP/2"
-            alt_svc = res.headers.get("Alt-Svc", "")
-            http3_support = "h3" in alt_svc or http_version == "HTTP/3"
-            
-            return HTTPVersionInfo(
-                http2=http2_support,
-                http3=http3_support
-            )
+        async with httpx.AsyncClient(http2=True, timeout=7) as client:
+            res = await client.get(url)
+            http2 = res.http_version == "HTTP/2"
+            alt_svc = res.headers.get("alt-svc","")
+            http3 = "h3" in alt_svc
+            return HTTPVersionInfo(http2=http2, http3=http3)
     except Exception:
-        try:
-            async with httpx.AsyncClient(http2=True, timeout=5, follow_redirects=True) as client:
-                res = await client.get(f"http://{domain}")
-                http_version = res.http_version
-                http2_support = http_version == "HTTP/2"
-                alt_svc = res.headers.get("Alt-Svc", "")
-                http3_support = "h3" in alt_svc or http_version == "HTTP/3"
-                return HTTPVersionInfo(
-                    http2=http2_support,
-                    http3=http3_support
-                )
-        except Exception:
-            return HTTPVersionInfo()
+        return HTTPVersionInfo()
